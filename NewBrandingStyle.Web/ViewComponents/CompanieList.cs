72e216c3683ee627b891831186c1e97e939dc4a0
IronManV <pawel.gawin98@gmail.com>
2020-12-03 20:36:55 +0100

    Implement companies list

---
+++ NewBrandingStyle.Web/ViewComponents/CompanieList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Database;
+
+namespace NewBrandingStyle.Web.ViewComponents
+{
+    public class CompanieList : ViewComponent
+    {
+        private readonly CompanyDbContext _dbContext;
+
+        public CompanieList(CompanyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var companies = _dbContext.Items.ToList();
+
+            return View("Index", companies);
+        }
+    }
+}
